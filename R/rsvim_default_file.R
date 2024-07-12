c0c741640ef9ae198d95ef3aa85492aaf8970593
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-07-12 22:34:19 +0100

    Add rsvim default file path

---
+++ R/rsvim_default_file.R
@@ -0,0 +1,22 @@
+#' Get suggested config file path
+#'
+#' The file is `.rstudiovim` located in
+#' `%APPDATA%/RStudio/keybindings/` where you can also find RStudio's JSON
+#' keybinding config files.
+#'
+#' @return String. The full file path to `.rstudiovim`
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' file.edit(rsvim_default_file())
+#' }
+rsvim_default_file <- function() {
+  file.path(
+    Sys.getenv("APPDATA"),
+    "RStudio",
+    "keybindings",
+    ".rstudiovim"
+  )
+}
