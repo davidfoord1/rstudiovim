8950593ff359c6dfbd9f38e7855357e4678e33d7
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-07-13 09:20:40 +0100

    Update rsvim_exec docs

--- R/rsvim_exec.R
+++ R/rsvim_exec.R
@@ -1,29 +1,41 @@
 #' Execute vim command in source editor
 #'
-#' Pass a string to be executed as vim command. [`KeyboardSimulator`] is used
-#' to simulate navigating to source, and typing
+#' Pass a string to be executed as a Vim command in the RStudio source editor
+#' [`KeyboardSimulator`] is used to simulate navigating to source, entering
+#' normal mode and typing out the command.
+#'
+#' @section Conditions:
+#' Of course Vim keybindings must be enabled and you must have a file open in
+#' the source editor so that you could execute the commands if you moved focus
+#' to source yourself.
 #'
 #' @param command String. A vim command to execute. This is the text you would
-#'   type in the vim command dialogue box after pressing `:` and before
-#'   pressing `Enter`.
+#'   type in the vim command dialogue box after pressing `:` and before pressing
+#'   `Enter`.
 #'
-#' @param focus_source String. Keyboard shortcut for focusing on the source
-#'   pane, e.g. as in Tools --> Modify Keyboard Shortcuts.... The RStudio
-#'   default is `Ctrl+1`. Build strings as appropriate for
-#'   [KeyboardSimulator::keybd.press()].
+#' @param focus_source String. Keyboard shortcut for `Move focus to source` e.g.
+#'   as in `Tools -> Modify Keyboard Shortcuts...` The RStudio default is
+#'   `Ctrl+1`.
 #'
-#' @return
-#' Returns `NULL` invisibly.
+#' @details Strings for `focus_source` must be built as appropriate for
+#' [KeyboardSimulator::keybd.press()].
+#'
+#' @return Returns `NULL` invisibly.
 #' @export
 #'
+#' @seealso [rsvim_exec_file()] to run all commands from a config file.
+#'
 #' @examples
-#' # Create a home-row binding jk to go from insert mode to normal mode.
+#' \dontrun{
+#' #' # Create a home-row binding jk to go from insert mode to normal mode.
 #' rsvim_exec("imap jk <Esc>")
 #'
 #' # Replace "text" with "rep" everywhere in the active document.
 #' rsvim_exec("%s/text/rep/g")
 #'
-#'
+#' # Bring up RStudio's Vim help (see Ex Commands for other commands)
+#' rsvim_exec("help")
+#' }
 rsvim_exec <- function(command, focus_source = "Ctrl+1") {
   KeyboardSimulator::keybd.press(focus_source)
   KeyboardSimulator::keybd.press("Esc")
