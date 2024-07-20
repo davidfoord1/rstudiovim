e9c977bdf397ecd00abfb0ba0e5ca080f2281cdb
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-07-20 14:10:25 +0100

    Update commands dataframe documentation

--- R/rstudio_vim_commands.R
+++ R/rstudio_vim_commands.R
@@ -1,11 +1,11 @@
-#' RStudio Vim Commands
+#' RStudio Vim Commands data.frame
 #'
 #' A data.frame of commands valid for use taken from the RStudio Vim key-binding
 #' javascript file. See pre-processing in `create_rstudio_vim_commands.R`. This
 #' is not a fully comprehensive list as it doesn't include commands like `help`
 #' and `R`.
 #'
-#' @format ## `rstudio_vim_commands`
+#' @format
 #' A data frame with < 50 rows and 2 columns:
 #' \describe{
 #'   \item{name}{Full command name}
