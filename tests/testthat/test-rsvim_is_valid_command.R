c9f3247568c1ea0ea9c35251a77ef6154656f4d1
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-07-20 14:31:50 +0100

    Create test-rsvim_is_valid_command.R

---
+++ tests/testthat/test-rsvim_is_valid_command.R
@@ -0,0 +1,16 @@
+test_that("rsvim_is_valid_command correctly validates commands", {
+  expect_true(rsvim_is_valid_command("map"))
+  expect_true(rsvim_is_valid_command("imap"))
+  expect_true(rsvim_is_valid_command("nmap"))
+  expect_true(rsvim_is_valid_command("vmap"))
+  expect_true(rsvim_is_valid_command("noremap"))
+  expect_true(rsvim_is_valid_command("inoremap"))
+  expect_true(rsvim_is_valid_command("nnoremap"))
+  expect_true(rsvim_is_valid_command("vnoremap"))
+
+  expect_false(rsvim_is_valid_command("hello"))
+  expect_false(rsvim_is_valid_command("world"))
+  expect_false(rsvim_is_valid_command(""))
+  expect_false(rsvim_is_valid_command(1))
+  expect_false(rsvim_is_valid_command(NA_character_))
+})
