4ba1a50287e8831381359af504b1e1ece3e2428e
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-07-26 12:53:17 +0100

    Update example_path to point to 2 examples

--- R/rsvim_example_path.R
+++ R/rsvim_example_path.R
@@ -1,16 +1,21 @@
 #' Path to rstudiovim example config
 #'
-#' Get the path to the `example.virmc` in the installed package.
-#' This file is used as the default template for
+#' Get the path to the example `.vimrc`s in the installed package.
+#' The basic file is used as the default template for
 #' [rsvim_use_template()]. You can use
 #' this if you want to review the file in your editor.
-#' You can also view the file online at
-#' [GitHub example.virmc](https://github.com/davidfoord1/rstudiovim/blob/main/inst/example.vimrc)
+#' You can also view the files online in the GitHub
+#' [inst folder](https://github.com/davidfoord1/rstudiovim/blob/main/inst/).
+#'
+#' @param name String. Name of the example file to find the path to. 2 are
+#' provided, "basic" and "full_example".
 #'
 #' @return
 #' String. The full path to the example file.
 #' @export
 #'
-rsvim_example_path <- function() {
-  system.file("example.vimrc", package = "rstudiovim")
+rsvim_example_path <- function(name = c("basic", "full_example")) {
+  name = match.arg(name)
+
+  system.file(paste0(name, ".vimrc"), package = "rstudiovim")
 }
