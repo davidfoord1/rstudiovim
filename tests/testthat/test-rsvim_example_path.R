257c48ed58f167edf337191463d9e91d9d048aac
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-07-26 12:54:15 +0100

    Update tests for 2 example paths

--- tests/testthat/test-rsvim_example_path.R
+++ tests/testthat/test-rsvim_example_path.R
@@ -1,7 +1,16 @@
 test_that("multiplication works", {
   expect_no_error(rsvim_example_path())
 
-  result <- rsvim_example_path()
+  default <- rsvim_example_path()
+  expect_true(basename(default) == "basic.vimrc")
 
-  expect_true(basename(result) == "basic.vimrc")
+  basic <- rsvim_example_path("basic")
+  expect_true(basename(default) == "basic.vimrc")
+
+  full <- rsvim_example_path("full_example")
+  expect_true(basename(full) == "full_example.vimrc")
+
+  expect_error(rsvim_example_path("not an example"))
+  expect_error(rsvim_example_path(NA_character_))
+  expect_error(rsvim_example_path(1))
 })
